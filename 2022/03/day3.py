999331ed525eb0e34e8ace4a28f42a31eef36949
samoshan <46390361+samoshan@users.noreply.github.com>
2022-12-04 17:05:31 +0000

    Update 2022 Day 3

--- 2022/03/day3.py
+++ 2022/03/day3.py
@@ -1,5 +1,4 @@
-from string import ascii_letters as letters
-sacks = open('input.txt').read().splitlines()
-part1 = [letters.index((set(sack[:len(sack)//2]) & set(sack[len(sack)//2:])).pop())+1 for sack in sacks]
-part2 = [letters.index(set.intersection(*map(set, sacks[x*3:x*3+3])).pop())+1 for x in range(len(sacks)//3)]
-print(f'Part 1: {sum(part1)}\nPart 2: {sum(part2)}')
+sacks, letters = open('input.txt').read().splitlines(), ' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+p1 = [letters.index((set(sack[:len(sack)//2]) & set(sack[len(sack)//2:])).pop()) for sack in sacks]
+p2 = [letters.index(set.intersection(*map(set, sacks[x*3:x*3+3])).pop()) for x in range(len(sacks)//3)]
+print(f'Part 1: {sum(p1)}\nPart 2: {sum(p2)}')
