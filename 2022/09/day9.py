5933dfdd021a16834816c4b008377886627d3a0b
samoshan <46390361+samoshan@users.noreply.github.com>
2022-12-09 14:45:28 +0000

    Update 2022 Day 9

--- 2022/09/day9.py
+++ 2022/09/day9.py
@@ -1,9 +1,9 @@
-from operator import add,sub; knots, visited, dirs = [(0,0),]*10, [{(0,0)},{(0,0)}], {'L':(-1,0),'U':(0,-1),'R':(1,0),'D':(0,1)}
+knots, visited, dirs = [0]*10, [{0}, {0}], {'L': -1, 'U': -1j, 'R': 1, 'D': 1j}
 for d, n in [((l := line.split(' '))[0], int(l[1])) for line in open('input.txt').read().splitlines()]:
   for _ in range(n):
-    knots[0] = tuple(map(add, knots[0], dirs[d]))
+    knots[0] = knots[0] + dirs[d]
     for i,knot in enumerate(knots[1:],1):
-      if abs(knot[0]-knots[i-1][0]) > 1 or abs(knot[1]-knots[i-1][1]) > 1:
-        knots[i] = tuple(map(add, knot, map(lambda x: 0 if x==0 else x/abs(x), map(sub, knots[i-1], knot))))
+      if abs(knot-knots[i-1]) >= 2:
+        knots[i] = knot + complex(((c := knots[i-1]-knot).real>0)-(c.real<0),(c.imag>0)-(c.imag<0))
     list(map(lambda v, k: v.add(knots[k]), visited, [1,-1]))
 print(*map(len, visited))
