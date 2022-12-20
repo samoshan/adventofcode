03d7654417da2c8faa7c1d26015b99907524c4ba
samoshan <46390361+samoshan@users.noreply.github.com>
2022-12-20 01:25:52 +0000

    2022 Day 19

---
+++ 2022/19/day19.py
@@ -0,0 +1,10 @@
+import re, math; data = open('input.txt').readlines()
+blueprints=[[(a,0,0,0),(b,0,0,0),(c,d,0,0),(e,0,f,0)] for a,b,c,d,e,f in [map(int,re.findall(r'\d+',line)[1:]) for line in data]]
+def dfs(recipes, time_left, resources=(0,0,0,0), robots=(1,0,0,0), best=0):
+  for (robot,recipe) in [x for x in enumerate(recipes) if all([robots[i]>0 for i,quantity in enumerate(x[1]) if quantity>0])]:
+    if robot==3 or (robots[robot] < (max_spend:=max(r[robot] for r in recipes)) and max_spend*time_left>resources[robot]):
+      if (wait:=1+max(0,max(math.ceil((qty-resources[i])/robots[i]) for i,qty in enumerate(recipe) if qty>0))) <= time_left:
+        new_res = tuple(resources[i] + robots[i]*wait - qty for i,qty in enumerate(recipe))
+        best=dfs(recipes,time_left-wait,new_res,tuple(robots[i]+1 if robot==i else robots[i] for i in range(4)), max(best,new_res[3]))
+  resources = tuple(resources[i] + robots[i]*(time_left+1) for i in range(4)); return max(best, resources[3])
+print(sum(i*dfs(bp, 23) for i,bp in enumerate(blueprints,1))); print(math.prod(dfs(bp, 31) for bp in blueprints[0:3]))
--- 2022/README.md
+++ 2022/README.md
@@ -22,7 +22,7 @@ For 2022 I am using Python with a focus on trying to write a low number of lines
 | 16 | [Proboscidea Volcanium](https://adventofcode.com/2022/day/16) | ⭐⭐ | [day16.py](16/day16.py) | 11 |
 | 17 |
 | 18 | [Boiling Boulders](https://adventofcode.com/2022/day/18) | ⭐⭐ | [day18.py](18/day18.py) | 8 |
-| 19 |
+| 19 | [Not Enough Minerals](https://adventofcode.com/2022/day/19) | ⭐⭐ | [day19.py](19/day19.py) | 10 |
 | 20 |
 | 21 |
 | 22 |
